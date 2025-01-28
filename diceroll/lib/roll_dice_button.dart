abcffede31da3acc15ecd57ac1af340ff1763cf5
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-29 02:46:39 +0530

    roll_dice_button draft created

---
+++ diceroll/lib/roll_dice_button.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: camel_case_types
+import 'package:flutter/material.dart';
+class roll_dice_button extends StatefulWidget
+{
+    const roll_dice_button({super.key});
+    @override
+    State<roll_dice_button> createState() => _roll_dice_button_state();
+}
+class _roll_dice_button_state extends State<roll_dice_button>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return (
+          Text('hello')
+    );
+  }
+}
