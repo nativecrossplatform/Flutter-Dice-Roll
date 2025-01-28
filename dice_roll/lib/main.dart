5293ce70ac3c6216f33ee169d80c3f00d7be8b0c
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-28 09:51:52 +0530

    formatting document

--- dice_roll/lib/main.dart
+++ dice_roll/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(const MaterialApp(home:Text('Hello World'),debugShowCheckedModeBanner: false));
+  runApp(const MaterialApp(home:Scaffold(
+
+  ),
+  debugShowCheckedModeBanner: false));
 }
 
 int randomGenerator() {
