789de398763a52b10188750d33b2869dec923219
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-29 13:36:09 +0530

    dice roll functionality almost ready

--- diceroll/lib/Widgets/dice_roll.dart
+++ diceroll/lib/Widgets/dice_roll.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-class DiceRoll extends StatefulWidget
-{
-    const DiceRoll({super.key});
-    State<DiceRoll> createState() => _DiceRollState();
+
+class DiceRoll extends StatefulWidget {
+  const DiceRoll({super.key});
+  @override
+  State<DiceRoll> createState() => _DiceRollState();
 }
-class _DiceRollState extends State<DiceRoll>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Column();
-    }
+
+class _DiceRollState extends State<DiceRoll> {
+  @override
+  Widget build(BuildContext context) {
+    String path = '';
+    return Column(
+      children: [
+        Image.asset(path),
+        OutlinedButton(
+            onPressed: ()=>{setState(() {path = '';})}, child: Text('ROLL THE DICE !'))
+      ],
+    );
+  }
 }
