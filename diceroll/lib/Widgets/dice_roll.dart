a787d90fcb54be266f864c6dbb67018c9e7a6345
Jash Upadhyay <jashupadhyay.work@gmail.com>
2025-01-29 17:19:37 +0530

    Final Project Completion

--- diceroll/lib/Widgets/dice_roll.dart
+++ diceroll/lib/Widgets/dice_roll.dart
@@ -7,15 +7,15 @@ class DiceRoll extends StatefulWidget {
 }
 
 class _DiceRollState extends State<DiceRoll> {
+  String path = 'assets/dice-images/dice-1.png';
   @override
   Widget build(BuildContext context) {
-    String path = 'assets/dice-images/dice-1.png';
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Image.asset(path),
         OutlinedButton(
-            onPressed: ()=>{setState(() {path = 'assets/dice-images/dice-$randomDice().png';})}, child: Text('ROLL THE DICE !',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),))
+            onPressed: ()=>{setState(() {path = 'assets/dice-images/dice-${randomDice()}.png';})}, child: Text('ROLL THE DICE !',style: TextStyle(color: Colors.white,fontWeight:FontWeight.bold),))
       ],
     );
   }
